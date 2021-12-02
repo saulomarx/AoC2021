f263d76c8059dc81a3425ef06bcdace3ce9f492a
saulomarx <saulo.paiva@contaquanto.com.br>
2021-12-02 12:25:35 -0300

    Day 01: adds first exercise

---
+++ days/day01/ex01.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	inputs := readInput()
+	messure := convertInputToInt(inputs)
+
+	var howDeepWeGo = 0
+	for i, el := range messure {
+		if i != 0 {
+			if el > messure[i-1] {
+				howDeepWeGo++
+			}
+		}
+	}
+	fmt.Println(howDeepWeGo)
+
+}
+
+func readInput() []string {
+	var input []string
+	file, err := os.Open("./inputs/day01/input.txt")
+	if err != nil {
+		fmt.Println("Deu ruim")
+	}
+	leitor := bufio.NewReader(file)
+
+	for {
+		line, err := leitor.ReadString('\n')
+		if err == io.EOF {
+			break
+		}
+		line = strings.TrimSpace(line)
+
+		input = append(input, line)
+	}
+
+	return input
+}
+
+func convertInputToInt(input []string) []int {
+	var response []int
+	for _, el := range input {
+		value, err := strconv.Atoi(el)
+
+		if err == nil {
+			response = append(response, value)
+		}
+
+	}
+	return response
+}
