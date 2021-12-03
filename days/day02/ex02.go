262da8194eac941529a3b8d91136a1ff91f5e688
saulomarx <saulo.paiva@contaquanto.com.br>
2021-12-03 11:15:53 -0300

    Day 02: exercise 2

---
+++ days/day02/ex02.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	inputs := readInput()
+	horizontal, deep := calcCoordinates(inputs)
+
+	fmt.Println(horizontal * deep)
+
+}
+
+func readInput() []string {
+	var input []string
+	file, err := os.Open("./inputs/day02/input.txt")
+	if err != nil {
+		fmt.Println("Deu ruim")
+	}
+	leitor := bufio.NewReader(file)
+
+	for {
+		line, err := leitor.ReadString('\n')
+		if err == io.EOF {
+			break
+		}
+		line = strings.TrimSpace(line)
+
+		input = append(input, line)
+	}
+
+	return input
+}
+
+func calcCoordinates(input []string) (horizontal, deep int) {
+	aim := 0
+
+	for _, el := range input {
+		slicedValue := strings.Split(el, " ")
+		key := slicedValue[0]
+
+		value, err := strconv.Atoi(slicedValue[1])
+
+		if err == nil {
+			if key == "down" {
+				aim = aim + value
+
+			} else if key == "up" {
+				aim = aim - value
+
+			} else if key == "forward" {
+				fmt.Println("ain ->", aim, "deep ->", deep, "+", aim, "*", value, "=", deep+aim*value, "|", aim, "*", value, "=", aim*value)
+
+				horizontal = horizontal + value
+				deep = deep + aim*value
+			}
+		}
+
+	}
+	fmt.Println(horizontal, deep)
+	return
+}
