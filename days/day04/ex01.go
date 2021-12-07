038008f7943a5ef927aa5d85c890acde88755e74
saulomarx <saulo.paiva@contaquanto.com.br>
2021-12-07 16:39:52 -0300

    Day 04: ex 01 and 02

---
+++ days/day04/ex01.go
@@ -0,0 +1,169 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var line = [5]bool{false, false, false, false, false}
+var matrix = [5][5]bool{line, line, line, line, line}
+
+type bingoBoard struct {
+	board       [5][5]int
+	resultBoard [5][5]bool
+	sum         int
+}
+
+func createBoard(board [5][5]int, sum int) *bingoBoard {
+	b := bingoBoard{board: board, resultBoard: matrix, sum: sum}
+	return &b
+}
+
+func main() {
+	inputs := readInput()
+	numbersString, boards := getNumbersAndBoards(inputs)
+
+	boardMap, numberToMap := mapBoards(boards)
+	numbers := convertStringLineToNumbers(numbersString)
+
+	playBingo(numbers, numberToMap, boardMap)
+}
+
+func playBingo(numbers []int, numberToMap map[int][]int, boardMap map[int]*bingoBoard) {
+	for round, number := range numbers {
+		fmt.Println("Round -> ", round+1, "| number ->", number)
+		boardsWithNumber := numberToMap[number]
+
+		for _, boardIndex := range boardsWithNumber {
+			lineIndex, columnIndex := findNumberPosition(boardMap[boardIndex].board, number)
+			boardMap[boardIndex].resultBoard[lineIndex][columnIndex] = true
+
+			boardMap[boardIndex].sum -= number
+			columWin := checkColumn(boardMap[boardIndex].resultBoard, columnIndex)
+			lineWin := checkLine(boardMap[boardIndex].resultBoard, lineIndex)
+			fmt.Println(lineWin, columWin)
+
+			if columWin || lineWin {
+				fmt.Println("Winner! Board ", boardIndex)
+				fmt.Println(boardMap[boardIndex].sum, number, boardMap[boardIndex].sum*number)
+				return
+			}
+		}
+	}
+
+}
+
+func readInput() []string {
+	var input []string
+	file, err := os.Open("./inputs/day04/input.txt")
+	if err != nil {
+		fmt.Println("Deu ruim")
+	}
+	leitor := bufio.NewReader(file)
+
+	for {
+		line, err := leitor.ReadString('\n')
+		if err == io.EOF {
+			break
+		}
+		line = strings.TrimSpace(line)
+
+		input = append(input, line)
+	}
+
+	return input
+}
+
+func checkColumn(matrix [5][5]bool, column int) bool {
+	result := true
+	for i := range matrix {
+		result = result && matrix[i][column]
+	}
+	return result
+}
+
+func checkLine(matrix [5][5]bool, line int) bool {
+	result := true
+	for j := range matrix {
+		result = result && matrix[line][j]
+	}
+	return result
+}
+
+func findNumberPosition(board [5][5]int, number int) (lineIndex, columIndex int) {
+	for i, line := range board {
+		for j, el := range line {
+			if el == number {
+				return i, j
+			}
+		}
+	}
+	return -1, -1
+}
+
+func getNumbersAndBoards(input []string) (numbers string, boards [][]string) {
+	numbers = input[0]
+	input = input[1:]
+
+	var board []string
+
+	for i, line := range input {
+		mod := i % 6
+		if mod > 0 {
+			board = append(board, line)
+		}
+		if mod == 5 {
+			boards = append(boards, board)
+			board = nil
+		}
+	}
+
+	return
+}
+
+func convertStringLineToNumbers(input string) []int {
+	var numbersList []int
+	stringNumbers := strings.Split(input, ",")
+
+	for _, stringNumber := range stringNumbers {
+		value, _ := strconv.Atoi(stringNumber)
+		numbersList = append(numbersList, value)
+
+	}
+	return numbersList
+}
+
+func mapBoards(boards [][]string) (boardMap map[int]*bingoBoard, numberToMap map[int][]int) {
+	boardMap = make(map[int]*bingoBoard)
+	numberToMap = make(map[int][]int)
+	for boardIndex, board := range boards {
+		var newBoard [5][5]int
+		var sum int
+		for lineIndex, line := range board {
+
+			numbersRaw := strings.Split(line, " ")
+			var lineNumbers []int
+			for _, stringNumber := range numbersRaw {
+				if stringNumber != "" {
+					value, _ := strconv.Atoi(stringNumber)
+					lineNumbers = append(lineNumbers, value)
+				}
+			}
+
+			for columnIndex, number := range lineNumbers {
+				newBoard[lineIndex][columnIndex] = number
+				numberToMap[number] = append(numberToMap[number], boardIndex)
+				sum += number
+			}
+
+		}
+		boardMap[boardIndex] = createBoard(newBoard, sum)
+
+	}
+
+	return
+}
